aa5ef4100852aec03657226a661990ac03eecbd1
Demi <70948778+ZixingLiu@users.noreply.github.com>
2022-07-15 15:08:21 -0700

    drag and move

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -15,22 +15,40 @@ public class PlayerControl : MonoBehaviour
     Vector3 startPoint;
     Vector3 endPoint;
 
+    Vector3 lastVelocity;
+
+    TrajectoryLine tl;
 
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+    }
+
+    private void Awake()
+    {
         rb = GetComponent<Rigidbody2D>();
+        tl = GetComponent<TrajectoryLine>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        lastVelocity = rb.velocity;
+
         if(Input.GetMouseButtonDown(0))
         {
             startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             startPoint.z = 15f;
             Debug.Log(startPoint);
+        }
+
+        if(Input.GetMouseButton(0))
+        {
+            Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            currentPoint.z = 15f;
+
+            tl.RenderLine(startPoint, currentPoint);
         }
 
         if(Input.GetMouseButtonUp(0))
@@ -42,6 +60,14 @@ public class PlayerControl : MonoBehaviour
                 Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
 
             rb.AddForce(force * power, ForceMode2D.Impulse);
+            tl.EndLine();
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float speed = lastVelocity.magnitude;
+        Vector3 direction = Vector3.Reflect(lastVelocity.normalized, collision.contacts[0].normal);
+        rb.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
