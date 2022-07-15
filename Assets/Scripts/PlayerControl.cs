021878a560a2210326ce3ec8c50f27ba27285ae7
Demi <70948778+ZixingLiu@users.noreply.github.com>
2022-07-15 14:41:38 -0700

    unfinished drag

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float power = 10f;
+    public Rigidbody2D rb;
+
+    public Vector2 minPower;
+    public Vector2 maxPower;
+
+    Camera cam;
+    Vector2 force;
+    Vector3 startPoint;
+    Vector3 endPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            startPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            startPoint.z = 15f;
+            Debug.Log(startPoint);
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+            endPoint.z = 15f;
+
+            force = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minPower.x, maxPower.x),
+                Mathf.Clamp(startPoint.y - endPoint.y, minPower.y, maxPower.y));
+
+            rb.AddForce(force * power, ForceMode2D.Impulse);
+        }
+    }
+}
