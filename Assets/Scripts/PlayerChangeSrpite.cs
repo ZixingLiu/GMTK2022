58810137c64fffdb385737086a7523005ff2ea7f
Demi <70948778+ZixingLiu@users.noreply.github.com>
2022-07-16 11:23:17 -0700

    player roll

--- Assets/Scripts/PlayerChangeSrpite.cs
+++ Assets/Scripts/PlayerChangeSrpite.cs
@@ -8,6 +8,8 @@ public class PlayerChangeSrpite : MonoBehaviour
     public Sprite[] sprite;
     public Sprite normalSprite;
 
+    public bool canChangeSprite = false;
+
 
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
@@ -27,12 +29,14 @@ public class PlayerChangeSrpite : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(rb.velocity.magnitude >0.2f)
+        if(rb.velocity.magnitude >1f)
         {
+            canChangeSprite = true;
             StartCoroutine(ChangeSprite());
         }
         else
         {
+            canChangeSprite = false;
             StopCoroutine(ChangeSprite());
             spriteRenderer.sprite = normalSprite;
         }
@@ -44,13 +48,16 @@ public class PlayerChangeSrpite : MonoBehaviour
 
         int randomDiceSide = 0;
 
-        for (int i = 0; i <= 20; i++)
+        while(canChangeSprite)
         {
             randomDiceSide = Random.Range(0, sprite.Length);
 
             spriteRenderer.sprite = sprite[randomDiceSide];
+            //int randomSide = 
 
-            yield return new WaitForSeconds(0.3f);
+            yield return new WaitForSeconds(1.2f);
+
         }
+        
     }
 }
